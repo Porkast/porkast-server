854ab1769d6961dfea6f810c50fb227f3324c714
chenjunqian <chenjunqian0810@foxmail.com>
2023-04-03 23:43:35 +0800

    feat: gen model and dao

---
+++ internal/dao/feed_dto.go
@@ -0,0 +1,44 @@
+package dao
+
+import "github.com/gogf/gf/v2/os/gtime"
+
+type FeedItem struct {
+	Id              string
+	ChannelId       string
+	Title           string
+	Link            string
+	PubDate         *gtime.Time
+	Author          string
+	InputDate       *gtime.Time
+	ImageUrl        string
+	EnclosureUrl    string
+	EnclosureType   string
+	EnclosureLength string
+	Duration        string
+	Episode         string
+	Explicit        string
+	Season          string
+	EpisodeType     string
+	Description     string
+	ChannelImageUrl string
+	ChannelTitle    string
+	Count           int
+	HasThumbnail    bool
+}
+
+type FeedChannel struct {
+	Id          string
+	Title       string
+	ChannelDesc string
+	ImageUrl    string
+	Link        string
+	FeedLink    string
+	Launguage   string
+	FeedType    string
+	Categories  string
+	Author      string
+	OwnerName   string
+	OwnerEmail  string
+	Items       []FeedItem
+	Count       int
+}
