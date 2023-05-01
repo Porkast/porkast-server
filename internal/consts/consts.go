af183677398b8ab326f28eeef48dec5e74c43d62
chenjunqian <chenjunqian0810@foxmail.com>
2023-05-01 22:10:16 +0800

    feat: add consts

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,3 +1,13 @@
 package consts
 
+// template consts
+const APP_NAME = "name"
+const APP_NAME_KEY = "锅烧FM"
+
+const TOTAL_PAGE = "totalPage"
 const FEED_ITEMS = "feedItems"
+const CHANNEL_INFO = "channelInfo"
+const ITEM_INFO = "itemInfo"
+
+const SEARCH_KEY_WORD = "searchKeyword"
+const CURRENT_PAGE = "currentPage"
--- internal/ctls/index.go
+++ internal/ctls/index.go
@@ -11,7 +11,7 @@ import (
 
 func (ctl *controller) IndexTpl(req *ghttp.Request) {
 	req.Response.WriteTpl("index.html", g.Map{
-		"name": "锅烧FM",
+		consts.APP_NAME_KEY: consts.APP_NAME,
 	})
 	return
 }
@@ -30,17 +30,17 @@ func (ctl *controller) SearchResult(req *ghttp.Request) {
 	page = req.GetQuery("page").Int()
 	items, err = feedService.SearchFeedItemsByKeyword(req.Context(), searchKeyword, page, 10)
 	if err != nil {
-        //TODO: Add error page
+		//TODO: Add error page
 	}
 	if len(items) > 0 {
 		count = items[0].Count
 		totalPage = count / 10
 	}
 	req.Response.WriteTpl("search.html", g.Map{
-		"searchKeyword":   searchKeyword,
-		"currentPage":     page,
-		"totalPage":       totalPage,
-		consts.FEED_ITEMS: items,
+		consts.SEARCH_KEY_WORD: searchKeyword,
+		consts.CURRENT_PAGE:    page,
+		consts.TOTAL_PAGE:      totalPage,
+		consts.FEED_ITEMS:      items,
 	})
 }
 
@@ -54,11 +54,11 @@ func (ctl *controller) FeedChannelDetail(req *ghttp.Request) {
 	channelId = req.Get("id").String()
 	channelInfo, err = feedService.GetChannelInfoByChannelId(req.Context(), channelId)
 	if err != nil {
-        // TODO: redirect to error page
+		// TODO: redirect to error page
 	}
 	req.Response.WriteTpl("feed_channel.html", g.Map{
-		"channelInfo":     channelInfo,
-		consts.FEED_ITEMS: channelInfo.Items,
+		consts.CHANNEL_INFO: channelInfo,
+		consts.FEED_ITEMS:   channelInfo.Items,
 	})
 }
 
@@ -75,10 +75,10 @@ func (ctl *controller) FeedItemDetail(req *ghttp.Request) {
 	channelId = req.Get("channelId").String()
 	channelInfo, itemInfo, err = feedService.GetFeedItemByItemId(req.Context(), channelId, itemId)
 	if err != nil {
-        // TODO: redirect to error page
+		// TODO: redirect to error page
 	}
 	req.Response.WriteTpl("feed_item.html", g.Map{
-		"itemInfo":    itemInfo,
-		"channelInfo": channelInfo,
+		consts.ITEM_INFO:    itemInfo,
+		consts.CHANNEL_INFO: channelInfo,
 	})
 }
