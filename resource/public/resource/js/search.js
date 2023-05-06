d52a599b3ee3bc8190c285360745a0f8c51a6520
chenjunqian <chenjunqian0810@foxmail.com>
2023-05-07 02:19:41 +0800

    feat: responsive header

--- resource/public/resource/js/search.js
+++ resource/public/resource/js/search.js
@@ -2,6 +2,8 @@ $(function() {
     // let searchClearBtn = $("#searchClearBtn");
     let searchBtn = $("#homeSearchBtn");
     let searchInput = $("#searchInput");
+    let searchBtnBottom = $("#homeSearchBtnBottom");
+    let searchInputBottom = $("#searchInputBottom");
     searchInput.keypress(function(event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13') {
@@ -12,6 +14,18 @@ $(function() {
     });
     searchBtn.click(function() {
         let keyword = searchInput.val()
+        window.location.href = "/search?q=" + keyword + "&page=1";
+    });
+    searchInputBottom.keypress(function(event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            let keyword = $(this).val()
+            window.location.href = "/search?q=" + keyword + "&page=1";
+        }
+        event.stopPropagation();
+    });
+    searchBtnBottom.click(function() {
+        let keyword = searchInputBottom.val()
         window.location.href = "/search?q=" + keyword + "&page=1";
     });
 });
