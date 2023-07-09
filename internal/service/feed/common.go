1b09829b41b64e52c9db2ef53fa34851753bb98b
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-09 13:06:04 +0800

    fix: empty item title

--- internal/service/feed/common.go
+++ internal/service/feed/common.go
@@ -102,6 +102,8 @@ func formatItemTitle(title string) (formatTitle string) {
 
 	if gstr.Contains(title, "\"") {
         formatTitle = gstr.Replace(title, "\"", "`")
+	} else {
+		formatTitle = title
 	}
 
 	return
