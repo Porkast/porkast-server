2b2dccdb1c5cf636d0793d7f3e39495640b5f4f2
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-10 22:16:16 +0800

    feat: add cache test case

---
+++ internal/service/cache/cache_test.go
@@ -0,0 +1,41 @@
+package cache
+
+import (
+	"context"
+	"testing"
+
+	"github.com/gogf/gf/v2/os/gctx"
+)
+
+func TestSetCache(t *testing.T) {
+	type args struct {
+		ctx          context.Context
+		key          string
+		value        string
+		expireSecond int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Set test cache",
+			args: args{
+				ctx:          gctx.New(),
+				key:          "test_key",
+				value:        "test_value",
+				expireSecond: 60 * 60,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			InitCache(tt.args.ctx)
+			if err := SetCache(tt.args.ctx, tt.args.key, tt.args.value, tt.args.expireSecond); (err != nil) != tt.wantErr {
+				t.Errorf("SetCache() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
