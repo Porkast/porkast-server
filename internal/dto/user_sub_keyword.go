44902f255a058c9cbaf656b2771340af92854e14
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-16 00:09:23 +0800

    feat: add usr sub keyword related entities

---
+++ internal/dto/user_sub_keyword.go
@@ -0,0 +1,33 @@
+package dto
+
+type UserSubKeyword struct {
+	Id              string
+	UserId          string
+	Keyword         string
+	OrderByDate     int
+	ItemId          string
+	ChannelId       string
+	CreateDate      string
+	Title           string
+	Link            string
+	PubDate         string
+	Author          string
+	InputDate       string
+	ImageUrl        string
+	EnclosureUrl    string
+	EnclosureType   string
+	EnclosureLength string
+	Duration        string
+	Episode         string
+	Explicit        string
+	Season          string
+	EpisodeType     string
+	Description     string
+	TextDescription string
+	ChannelImageUrl string
+	ChannelTitle    string
+	ChannelAuthor   string
+	FeedLink        string
+	Count           int
+	HasThumbnail    bool
+}
