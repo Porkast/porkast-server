5eaf520cda5eb26bcf2720f245f5d270d2148edd
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-30 17:37:45 +0800

    feat: add staus const

--- internal/consts/listen_later.go
+++ internal/consts/listen_later.go
@@ -1,5 +1,10 @@
 package consts
 
+const (
+	LISTEN_LATER_ACTIVE int = iota
+	LISTEN_LATER_DELETED
+)
+
 const LISTEN_LATER_ITEM_LIST = "listen_later_item_list"
 const LISTEN_LATER_PLAYLIST_NAME = "listen_later_playlist_name"
 const LISTEN_LATER_PLAYLIST_NAME_TEXT = "播放列表"
--- internal/consts/subscription.go
+++ internal/consts/subscription.go
@@ -1,5 +1,10 @@
 package consts
 
+const (
+	SUB_KEYWORD_ACTIVE int = iota
+	SUB_KEYWORD_DELETED
+)
+
 const SUB_KEYWORD_EXIST = "sub_keyword_exist"
 const SUB_KEYWORD_SUCCESS = "sub_keyword_success"
 const SUB_KEYWORD_LANG = "sub_keyword_lang"
