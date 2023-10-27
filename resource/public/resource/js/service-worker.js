a1675aace4e34d25944e89b87e43da949c6474e7
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-27 22:45:35 +0800

    refactor: remove tpl

--- resource/public/resource/js/service-worker.js
+++
@@ -1,9 +0,0 @@
-/**
- * service worker
- */
-self.addEventListener("install", event => {
-    console.log("Service worker installed");
-});
-self.addEventListener("activate", event => {
-    console.log("Service worker activated");
-});
--- resource/public/resource/js/verify_auth.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    let userInfo = getUserInfo()
-    if (userInfo === undefined || userInfo === null) {
-        window.location.href = '/login'
-    }
-})
